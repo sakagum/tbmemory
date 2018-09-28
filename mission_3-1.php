c31746f248cc7ee6c23bfc065486b9e17ca3898c
sakagum <43466341+sakagum@users.noreply.github.com>
2018-09-28 12:08:51 +0900

    Update mission_3-1.php

--- mission_3-1.php
+++ mission_3-1.php
@@ -5,17 +5,15 @@
  </head>
   <body>
 
-<?php
+<?php>
 
-//ŽQÆ@www.iimc.kyoto-u.ac.jp/ja/services/whs/support/make_con/php_db.html
-
-//ƒT[ƒoÚ‘± 3-1
-$dsn = 'ƒf[ƒ^ƒx[ƒX–¼';
-$user = 'ƒ†[ƒU[–¼';
-$password = 'ƒpƒXƒ[ƒh';
+//ã‚µãƒ¼ãƒæŽ¥ç¶š 3-1
+$dsn = 'ãƒ‡ãƒ¼ã‚¿ãƒ™ãƒ¼ã‚¹å';
+$user = 'ãƒ¦ãƒ¼ã‚¶ãƒ¼å';
+$password = 'ãƒ‘ã‚¹ãƒ¯ãƒ¼ãƒ‰';
 $pdo = new PDO($dsn,$user,$password);
 
-//ƒe[ƒuƒ‹‚Ìì¬@3-2
+//ãƒ†ãƒ¼ãƒ–ãƒ«ã®ä½œæˆã€€3-2
 $sql = "CREATE TABLE tbtest" 
 ." (" 
 ."id INT," 
@@ -24,15 +22,15 @@ $sql = "CREATE TABLE tbtest"
 .");";
 $stmt = $pdo->query($sql);
 
-//tbtest\\\‚Á‚Ä‚Ìì‚é 3-3
-$sql = 'SHOW TABLES'; //î•ñ‚ðŒ©‚é
+//tbtestâ€•â€•â€•ã£ã¦ã®ä½œã‚‹ 3-3
+$sql = 'SHOW TABLES'; //æƒ…å ±ã‚’è¦‹ã‚‹
 $result = $pdo -> query($sql); 
  foreach ($result as $row){ echo $row[0]; 
  echo '<br>';
 }
 echo "<hr>";
 
-//tbtest‚Ì‰º‚ÌƒSƒbƒ`ƒƒƒSƒ`ƒƒ‚µ‚½‚â‚Â 3-4
+//tbtestã®ä¸‹ã®ã‚´ãƒƒãƒãƒ£ã‚´ãƒãƒ£ã—ãŸã‚„ã¤ 3-4
 $sql = 'SHOW CREATE TABLE tbtest';
 $result = $pdo->query($sql);
 var_dump($result);
@@ -43,14 +41,14 @@ echo "<hr>";
 
 //3-5
 $sql = $pdo -> prepare("INSERT INTO tbtest (id,name,comment) VALUES ('1',:name,:comment)");
-//INSERT‚Íî•ñ‚Ì’Ç‰Á
+//INSERTã¯æƒ…å ±ã®è¿½åŠ 
 $sql -> bindParam(':name',$name,PDO::PARAM_STR);
 $sql -> bindParam(':comment',$comment,PDO::PARAM_STR);
 $name='Tom';
 $comment='Hey';
 $sql -> execute();
 
-//ƒRƒƒ“ƒg•\Ž¦ 3-6
+//ã‚³ãƒ¡ãƒ³ãƒˆè¡¨ç¤º 3-6
 $sql = 'SELECT *FROM tbtest';
 $results = $pdo->query($sql);
 foreach ($results as $row ){
@@ -59,14 +57,14 @@ echo $row['name'].',';
 echo $row['comment'].'<br>';
 }
 
-//$id‚Í‘‚«Š·‚¦‚é”Žš@3-7
+//$idã¯æ›¸ãæ›ãˆã‚‹æ•°å­—ã€€3-7
 $id = 2;
 $nm = "Mike";
 $kome = "Howdy";
 $sql = "update tbtest set name='$nm' , comment='$kome' where id =$id";
 $result = $pdo->query($sql);
 
-//$id‚ÍÁ‚·”Žš@3-8
+//$idã¯æ¶ˆã™æ•°å­—ã€€3-8
 $id=2;
 $sql="delete from tbtest where id=$id";
 $result=$pdo->query($sql);
